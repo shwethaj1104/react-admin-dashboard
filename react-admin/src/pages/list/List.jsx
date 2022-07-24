a07480ad6212e5be56a58191c69df5f0688e7b94
shwethaj1104 <shwethaj1104@gmail.com>
2022-07-24 09:13:08 +0530

    changes for users section

--- react-admin/src/pages/list/List.jsx
+++ react-admin/src/pages/list/List.jsx
@@ -1,9 +1,18 @@
 import "./list.scss"
+import SideBar from "../../components/sidebar/SideBar"
+import NavBar from "../../components/navbar/NavBar"
+import DataTable from "../../components/datatable/DataTable"
 
 
 const List = () => {
   return (
-    <div className="list">List</div>
+    <div className="list">
+      <SideBar />
+      <div className="listContainer">
+        <NavBar />
+        <DataTable />
+      </div>
+    </div>
   )
 }
 
